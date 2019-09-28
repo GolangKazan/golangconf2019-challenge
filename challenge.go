9a6fd6511a4f6349e845459a7fc8fa25028f77af
Iskander Sharipov <i.sharipov@corp.vk.com>
2019-09-28 15:30:20 +0300

    initial commit
    Signed-off-by: Iskander Sharipov <i.sharipov@corp.vk.com>

---
+++ challenge.go
@@ -0,0 +1,44 @@
+package challenge
+
+// gophers is a list of GolangKazan GolangConf-2019 participants.
+//
+// Used in tests (see challenge_test.go).
+var gophers = []gopher{
+	{
+		name: "Iskander Sharipov",
+		id:   "quasilyte",
+		post: "https://todo.add.a.post/123",
+	},
+}
+
+// gopher is a struct that describes GolangKazan GolangConf-2019 challenge participant.
+type gopher struct {
+	// name is participant full name (first + second).
+	name string
+
+	// id is a secondary identifier that is used if name alone can't
+	// identify the participant. Optional, can be left empty.
+	id string
+
+	// post is a link to a media post about this challenge.
+	//
+	// We accept any kind of posts, here are some examples:
+	//	- VK or Facebook post on a wall
+	//	- Tweet
+	//	- Instagram post
+	//	- Etc, etc. (but read rules below beforehand)
+	//
+	// Rules:
+	//	1. Post must include both #GolangKazan #GolangConf2019 hash tags.
+	//	2. Post must survive until we stop the challenge and announce the winner.
+	//	3. Account that does the announcement must have at least 10 followers/subscribers.
+	post string
+}
+
+// key returns a hopefully unique participant key.
+func (g gopher) key() string {
+	if g.id == "" {
+		return g.name
+	}
+	return g.name + "/" + g.id
+}
---
+++ challenge_test.go
@@ -0,0 +1,52 @@
+package challenge
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestWinner(t *testing.T) {
+	// ignore is a set of people who don't/can't participate.
+	ignore := map[string]bool{
+		// "Iskander Sharipov/quasilyte": true,
+	}
+
+	// filtered is a list of participating gophers.
+	// Basically, a gophers list without ignored members.
+	filtered := []gopher{}
+
+	// keys is a set of all participants keys.
+	// Needed to detect collisions.
+	keys := map[string]bool{}
+
+	// Check whether all fields are set correctly.
+	for i, g := range gophers {
+		if g.name == "" {
+			t.Errorf("gopher[%d] name is empty", i)
+			continue
+		}
+		if g.post == "" {
+			t.Errorf("gopher[%d] (%s) post is empty", i, g.name)
+			continue
+		}
+
+		if ignore[g.key()] {
+			continue
+		}
+
+		if keys[g.key()] {
+			t.Errorf("gopher[%d] (%s) is a duplicate", i, g.name)
+			continue
+		}
+		keys[g.key()] = true
+
+		filtered = append(filtered, g)
+	}
+
+	if len(filtered) != 0 {
+		rand.Seed(time.Now().UTC().UnixNano())
+		winner := rand.Intn(len(filtered))
+		t.Logf("The winner is: %s!", filtered[winner].key())
+	}
+}
