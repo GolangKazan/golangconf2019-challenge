df4ed52ef208da6e58e3097fc1c6e9affb5e2800
Nur Kutlugallyamov <47988791+milQA@users.noreply.github.com>
2019-09-29 20:23:25 +0300

    del field tester for Nur Kutlugallyamov (#19)
    del field tester for Nur Kutlugallyamov

--- challenge.go
+++ challenge.go
@@ -45,6 +45,11 @@ var gophers = []gopher{
 		id:   "vadimgorbunov",
 		post: "https://twitter.com/vadimgorbunov/status/1178323050518974468",
 	},
+	{
+		name: "Nur Kutlugallyamov",
+		id:   "milQA",
+		post: "https://vk.com/wall302234537_938",
+	},
 
 	// Participants from Nizhny Novgorod (https://vk.com/golang_nizhny):
 
@@ -54,12 +59,6 @@ var gophers = []gopher{
 		name:   "Iskander Sharipov",
 		id:     "quasilyte",
 		post:   "https://todo.add.a.post/123",
-		tester: true,
-	},
-	{
-		name:   "Nur Kutlugallyamov",
-		id:     "milQA",
-		post:   "https://vk.com/wall302234537_938",
 		tester: true,
 	},
 	{
